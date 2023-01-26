dd6734a0b3be105fccd7108e471a9c88ef68abb7
alexwololo <alexander.caspersson@gmail.com>
2023-01-26 08:44:57 +0100

    reset agin

---
+++ README_ANT.md
@@ -0,0 +1,14 @@
+## ant design
+
+npm install ant-design-vue
+
+## ändra main.js
+
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+createApp(App).use(Antd).mount("#app");
+
+## ant icons
+
+npm install @ant-design/icons-vue
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <div class="center">
+    <rocket-two-tone two-tone-color="#eb2f96" :rotate="50" />
+    <reload-outlined spin />
+  </div>
+</template>
+
+<script setup>
+import { RocketTwoTone, ReloadOutlined } from "@ant-design/icons-vue";
+</script>
